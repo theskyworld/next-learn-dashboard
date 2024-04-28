d49964949fcb07a047350685904e420ced8b0405
theskyworld <2456450687@qq.com>
2024-04-28 12:28:03 +0800

    feat: finish chapter 5

--- src/app/NavLinks.tsx
+++ src/app/NavLinks.tsx
@@ -1,5 +1,8 @@
+"use client";
 import { HomeIcon, UserGroupIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
-
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import Link from "next/link";
 interface NavLinkType {
   name: string;
   href: string;
@@ -25,16 +28,26 @@ const links: NavLinkType[] = [
 ];
 
 export default function NavLinks() {
+  // 获取当前路由的路径
+  const pathName = usePathname();
+
   return (
     <>
       {
         links.map(link => {
           const LinkIcon = link.icon;
           return (
-            <a key={link.name} href={link.href} className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm text-[black] font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <Link key={link.name} href={link.href} className= {
+              clsx(
+                "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm text-[black] font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                {
+                  'bg-sky-100 text-blue-600':pathName === link.href
+                }
+              )
+            }>
               <LinkIcon className="w-6" />
               <p className="hidden md:block">{link.name}</p>
-            </a>
+            </Link>
           )
         })
       }
